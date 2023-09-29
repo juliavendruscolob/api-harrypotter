0243fb1277c4055ea76aed1c8ad05e9a8d4c8253
Julia Franciele Vendruscolo Bilhan <juliafrancielevbilhan@gmail.com>
2023-09-28 23:53:29 -0300

    fixing catch erros

--- src/main/java/springbootapi/springbootapiharrypotter/ElkLogger/ElkLoggerService.java
+++ src/main/java/springbootapi/springbootapiharrypotter/ElkLogger/ElkLoggerService.java
@@ -1,10 +1,12 @@
 package springbootapi.springbootapiharrypotter.ElkLogger;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
+import reactor.core.Exceptions;
 import reactor.core.publisher.Mono;
 import springbootapi.springbootapiharrypotter.ElkConfiguration.ElkLoggerConfig;
 
@@ -23,28 +25,23 @@ public class ElkLoggerService {
         this.elkLoggerConfig = elkLoggerConfig;
     }
 
-    public <T> Mono <T> ElkLogger(T document) {
+    public <T> Mono<T> ElkLogger(T document) {
         String authToken = elkLoggerConfig.getAuthToken();
         String elasticSearchIndex = elkLoggerConfig.getElasticSearchIndex();
 
-        return webClient
-                .post()
-                .uri(elasticSearchIndex)
-                .header("Authorization", "Basic " + authToken)
-                .body(BodyInserters.fromValue(document))
-                .retrieve()
-                .bodyToMono((Class<T>) document.getClass())
-                .onErrorResume(e -> {
-                    if (e instanceof WebClientResponseException) {
-                        WebClientResponseException responseException = (WebClientResponseException) e;
-                        System.err.println("Document indexed successfully - Status: " + responseException.getStatusCode());
-                    } else {
-                        System.err.println("Error indexing document " + e.getMessage());
-                    }
-                    return Mono.empty();
-                });
-
+        try {
+            webClient
+                    .post()
+                    .uri(elasticSearchIndex)
+                    .header("Authorization", "Basic " + authToken)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(BodyInserters.fromValue(document))
+                    .retrieve()
+                    .bodyToMono(Void.class)
+                    .block();
+        } catch (Exception e) {
+            System.err.println("Error indexing document: " + e.getMessage());
+        }
+        return Mono.empty();
     }
 }
-
-
